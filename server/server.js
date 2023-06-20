0f942dd2f1262eef1e2f907ef1618d143dd84a21
Surya-MERNstack <charusurya17@gmail.com>
2023-06-20 14:28:44 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -3,24 +3,28 @@ const app = express();
 const cors = require('cors');
 
 require('dotenv').config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 // use middleware
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 
 // mongodb connection
-const con = require('./db/connection.js');
+const con = require('./db/connection.js'); 
 
 // using routes
 app.use(require('./routes/route'));
+
+app.get('/' ,(req ,res) =>{
+     res.send('<h1>Hello World</h1>')
+})
 
 con.then(db => {
     if (!db) return process.exit(1);
 
     // listen to the http server
     app.listen(port, () => {
-        console.log(`Server is running on port: ${port}`);
+        console.log(`Server is running on port: http://localhost:${port}`);
     });
 
     app.on('error', err => {
